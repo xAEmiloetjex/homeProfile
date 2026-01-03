1e414886ef6a1f741c97b48dec04eeb711afef02
xAEmiloetjex <ssyt.amy@gmail.com>
2026-01-03 12:14:54 +0100

    add package.sh from v2.0.3 but now in correct path

---
+++ scripts/package.sh
@@ -0,0 +1,19 @@
+#!/usr/bin/env bash
+source "$HOME/homeProfile/globals.sh"
+
+PKGDIR=$HPROOT/pkg
+
+mv -f $PKGDIR/.git $HPROOT/.git
+rm -rf $PKGDIR
+
+mkdir -p $PKGDIR/profiles/default $PKGDIR/scripts
+mv -f $HPROOT/.git $PKGDIR/.git
+
+
+cp -r $HPDEFAULTPROF/config.sh $PKGDIR/profiles/default/config.sh
+cp -r $HPROOT/scripts $PKGDIR
+cp $HPROOT/globals.sh $PKGDIR/globals.sh
+cp $HPROOT/homeProfile.sh $PKGDIR/homeProfile.sh
+cp $HPROOT/README.md $PKGDIR/README.md
+cp $HPROOT/LICENSE $PKGDIR/LICENSE
+
